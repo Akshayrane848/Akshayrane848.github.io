13ae042d35c5088e59779515b8ab20fa0e703c15
Akshay Rane <akshayrane848@gmail.com>
2022-09-14 20:25:10 +0530

    add update

--- js/script.js
+++ js/script.js
@@ -9,5 +9,27 @@ $(document).ready(function () {
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
 
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+
     })
+
+    // smooth scrolling
+
+    $('a[href*="#"]').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('html,body').animate({
+
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
+            500,
+            'linear'
+
+        );
+    });
 });
